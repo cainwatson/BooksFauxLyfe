a605fd2c2aa2184d8b25df0cc175ee6eb4adc510
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2020-12-04 20:41:26 -0600

    Add book seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias BooksFauxLyfe.Works
+
+{:ok, _book1} =
+  Works.create_book(%{
+    date_published: ~D[2006-02-01],
+    edition: "First Harper Perennial Modern Classics Edition",
+    isbn: "9780060883287",
+    title: "One Hundred Years of Solitude"
+  })
+
+{:ok, _book2} =
+  Works.create_book(%{
+    date_published: ~D[2005-08-02],
+    edition: "40th Anniversary Edition",
+    isbn: "9780441013593",
+    title: "Dune"
+  })
