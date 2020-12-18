020db6448dd8f4dde7f9fc0fca9b4f4d8527e153
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2020-12-17 20:31:39 -0600

    Add linters to mix test alias

--- mix.exs
+++ mix.exs
@@ -62,7 +62,13 @@ defmodule BooksFauxLyfe.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      test: [
+        "format --check-formatted",
+        "credo --strict",
+        "ecto.create --quiet",
+        "ecto.migrate --quiet",
+        "test"
+      ],
       s: ["phx.server"]
     ]
   end
